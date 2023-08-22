e26683b1629a65e4a59449be47be23a8483b9d27
Shlyuha <YaShlyuhaa@gmail.com>
2023-08-23 00:02:24 +0300

    Initial commit

---
+++ src/main/java/com/example/imbabackend/ImbaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.imbabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImbaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImbaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imbabackend/ImbaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imbabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImbaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
